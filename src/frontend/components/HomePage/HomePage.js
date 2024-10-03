e03b3331fbaabb4894cc9a290b98ffe40528130d
Roberto Andino <andinorobert518@yahoo.com>
2024-10-03 14:08:44 -0400

    Added Home Page

---
+++ src/frontend/components/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './HomePage.css';
+
+const HomePage = () => {
+    return(
+        <div>
+            <h1>Welcome to Your Fitness Tracker</h1>
+            <p>Track your workouts and view your progress here.</p>
+        </div>
+    );
+};
+
+export default HomePage;
+
